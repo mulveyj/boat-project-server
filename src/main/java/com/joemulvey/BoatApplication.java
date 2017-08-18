62d587ed1db8a08821429fc191fb8564bdfddf81
mulveyj <github@joemulvey.com>
2017-08-18 14:13:03 +0100

    Initial Commit and setup

---
+++ src/main/java/com/joemulvey/BoatApplication.java
@@ -0,0 +1,12 @@
+package com.joemulvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatApplication.class, args);
+	}
+}
