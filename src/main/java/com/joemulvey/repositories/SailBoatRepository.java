6ca4adaa99314cecea96da88867d6f86e7a8b886
mulveyj <github@joemulvey.com>
2017-08-18 16:05:12 +0100

    SailBoatRepository interface

---
+++ src/main/java/com/joemulvey/repositories/SailBoatRepository.java
@@ -0,0 +1,8 @@
+package com.joemulvey.repositories;
+
+import com.joemulvey.models.SailBoat;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SailBoatRepository extends CrudRepository<SailBoat, Long> {}
+
+
