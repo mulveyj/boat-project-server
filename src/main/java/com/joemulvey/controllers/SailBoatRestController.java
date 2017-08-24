96b8e51618c578fd512b42454d25dc2e1de9e3d3
mulveyj <github@joemulvey.com>
2017-08-24 13:46:51 +0100

    logging

--- src/main/java/com/joemulvey/controllers/SailBoatRestController.java
+++ src/main/java/com/joemulvey/controllers/SailBoatRestController.java
@@ -4,11 +4,15 @@ import com.joemulvey.models.SailBoat;
 import com.joemulvey.repositories.SailBoatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RestController
 public class SailBoatRestController {
+
+    private final Logger logger = LoggerFactory.getLogger(SailBoatRestController.class);
 
     @Autowired
     protected SailBoatRepository sailBoatRepository;
@@ -21,7 +25,18 @@ public class SailBoatRestController {
     @CrossOrigin("*")
     @RequestMapping(value="/sailboats/{boat_id}", method=RequestMethod.GET)
     public SailBoat getSailBoat(@PathVariable Long boat_id) {
-        return sailBoatRepository.findOne(boat_id);
+
+        logger.debug("Starting getSailBoat");
+
+        SailBoat thisboat = sailBoatRepository.findOne(boat_id);
+
+        if (thisboat == null) {
+            logger.warn("No SailBoat with ID {}", boat_id);
+        }
+
+        logger.debug("Exiting getSailBoat");
+
+        return thisboat;
     }
 
 }
